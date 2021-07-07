6f52c65276200516feca88a4b59bbde81a86c78d
Edgar <mmnledgar@gmail.com>
2021-07-07 14:31:35 +0000

    modify angularjs models folder

--- public/app/services/services.js
+++ public/app/services/services.js
@@ -1,2 +1,2 @@
-document.writeln("<script type='text/javascript' src='app/models/Item/Item.js'></script>");
-document.writeln("<script type='text/javascript' src='app/models/Car/Car.js'></script>");
+document.writeln("<script type='text/javascript' src='app/models/Item.js'></script>");
+document.writeln("<script type='text/javascript' src='app/models/Car.js'></script>");
